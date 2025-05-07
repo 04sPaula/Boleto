53c26b99d8a2e5507840bf29c0cd8ea2a07229d2
Paula <138329199+04sPaula@users.noreply.github.com>
2025-05-07 18:57:12 -0300

    Adaptação dos argumentos pro bopepo (pt6)

--- src/Boleto.java
+++ src/Boleto.java
@@ -2,7 +2,9 @@ import java.util.Calendar;
 
 public interface Boleto {
     String getSacado();
+    String getSacadoCad();
     String getCedente();
+    String getCedenteCad();
     double getValor();
     Calendar getVencimento();
     int getNossoNumero();
