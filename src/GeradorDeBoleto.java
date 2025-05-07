d0780969eb35193da56c95ea837efe2c56a4d8b1
Paula <138329199+04sPaula@users.noreply.github.com>
2025-05-07 19:07:59 -0300

    Adaptação dos argumentos pro bopepo (pt9)

--- src/GeradorDeBoleto.java
+++ src/GeradorDeBoleto.java
@@ -3,20 +3,24 @@ import java.util.Calendar;
 public class GeradorDeBoleto {
     private final BoletoBuilder boletoBuilder;
     String sacado;
+    String sacadoCad;
     String cedente;
+    String cedenteCad;
     double valor;
     Calendar vencimento;
 
-    public GeradorDeBoleto(BoletoBuilder boletoBuilder, String sacado, String cedente, double Valor, Calendar Vencimento) {
+    public GeradorDeBoleto(BoletoBuilder boletoBuilder, String sacado, String sacadoCad, String cedente, String cedenteCad, double Valor, Calendar Vencimento) {
         this.boletoBuilder = boletoBuilder;
         this.sacado = sacado;
+        this.sacadoCad = sacadoCad;
         this.cedente = cedente;
+        this.cedenteCad = cedenteCad;
         this.valor = Valor;
         this.vencimento = Vencimento;
     }
     public Boleto geraBoleto() {
-        this.boletoBuilder.buildSacado(sacado);
-        this.boletoBuilder.buildCedente(cedente);
+        this.boletoBuilder.buildSacado(sacado, sacadoCad);
+        this.boletoBuilder.buildCedente(cedente, cedenteCad);
         this.boletoBuilder.buildValor(valor);
         this.boletoBuilder.buildVencimento(vencimento);
         this.boletoBuilder.buildNossoNumero();
